d1ff3237421f014df2911956804d09a4d564a478
kinotonik <zedmed@kinotonik.fr>
2023-03-07 09:56:55 +0100

    list des doctors

--- src/main/java/fr/wcs/doctor/controller/DoctorController.java
+++ src/main/java/fr/wcs/doctor/controller/DoctorController.java
@@ -1,2 +1,27 @@
-package fr.wcs.doctor.controller;public class DoctorController {
+package fr.wcs.doctor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String getDoctor1() {
+        return "Doc la Peluche";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String getDoctor10() {
+        return "Doc Jekyll";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String getDoctor13() {
+        return "Doc Frankenstein";
+    }
+
 }
